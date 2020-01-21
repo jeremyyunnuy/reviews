668f14cee72aefe5b21446e2d2c5c7b0413d3609
YUN HU <asdfg20232jp@gmail.com>
2020-01-21 18:40:49 +0800

    count avg

--- read.py
+++ read.py
@@ -4,10 +4,13 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1 # count = count + 1
-		if count % 1000 == 0: # %用來求餘數
+		if count % 10000 == 0: # %用來求餘數
 			print(len(data))
-print(len(data))
+print('檔案讀取完了, 總共有', len(data), '筆資料')
 
 print(data[0]) #印出清單第0個位置
-print('-----------------------')
-print(data[5229])
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('留言的平均長度為', sum_len/len(data))
