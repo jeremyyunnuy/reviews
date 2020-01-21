951c3e9cbeb5ddab38f75d734bde9560d177d917
YUN HU <asdfg20232jp@gmail.com>
2020-01-21 18:50:09 +0800

    filiter

--- read.py
+++ read.py
@@ -14,3 +14,11 @@ sum_len = 0
 for d in data:
 	sum_len += len(d)
 print('留言的平均長度為', sum_len/len(data))
+# 以下為篩選 小於100
+new = [] #新增新的清單  
+for d in data: # 把清單中的東西一個一個拿出來
+	if len(d) < 100:
+		new.append(d) # 加入到新的新單裡面
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
